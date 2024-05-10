d44f4c020240bb985936e8ca393c13137d1801dd
Scott Bedard <scottbedard90@gmail.com>
2024-05-10 15:51:02 -0500

    repro

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+function onEnter() {
+  console.log('enter')
+}
+
+function onKeydown() {
+  console.log('keydown')
+}
+</script>
+
+<template>
+  <input
+    @keydown="onKeydown"
+    @keydown.enter="onEnter" />
+</template>
